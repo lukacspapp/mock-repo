b37e0a93ce11b003f14c74fd3c973a13a7f0bc76
Lukacs Papp <lukacs.papp@jentis.com>
2024-07-22 20:12:34 +0100

    s

--- run_script.py
+++ run_script.py
@@ -3,6 +3,7 @@ import os
 import git
 
 from git_contributions_importer import *
+from datetime import datetime, timedelta
 
 # Clone the remote repository to a local folder
 url = "https://github.com/lukacspapp/mock-repo"
@@ -11,8 +12,8 @@ if not os.path.exists(local_path):
     git.Repo.clone_from(url, local_path)
 
 # Clone the private repository to a local folder
-private_repo_url = "https://bitbucket.org/jentis/admin-backend/src/master/"
-private_repo_local_path = "/Users/lukacspapp/JENTIS/admin-backend"  # Replace with your desired local path
+private_repo_url = "https://bitbucket.org/jentis/tracking/src/master/"
+private_repo_local_path = "/Users/lukacspapp/JENTIS/tracking"  # Replace with your desired local path
 if not os.path.exists(private_repo_local_path):
     git.Repo.clone_from(private_repo_url, private_repo_local_path)
 
@@ -21,6 +22,17 @@ repo = git.Repo(private_repo_local_path)
 # Your mock repo
 mock_repo = git.Repo(local_path)
 importer = Importer([repo], mock_repo)
+one_year_ago = int((datetime.now() - timedelta(days=365)).timestamp())
+importer.set_ignore_before_date(one_year_ago)
+
+# Do not collapse multiple changes into one
+importer.set_collapse_multiple_changes_to_one(False)
+
+# Allow the importer to shift commits up to a certain maximum past date
+importer.set_commit_time_max_past(365 * 24 * 60 * 60)  # up to 1 year in the past
+
+# Do not limit the number of changes per commit
+importer.set_commit_max_amount_changes(-1) # start importing from the last commit date in the mock repo
 
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
